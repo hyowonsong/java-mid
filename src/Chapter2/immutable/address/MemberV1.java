0e71ee087020fc019e229818a9a0f23ba92a695b
hyowonsong <woniwoni30@gmail.com>
2024-03-31 11:58:25 +0900

    [Add] Chapter2. 불변 객체 실습(return 값 추가)

---
+++ src/Chapter2/immutable/address/MemberV1.java
@@ -0,0 +1,28 @@
+package Chapter2.immutable.address;
+
+// 이렇게 만들면 사이드 이펙트 발생
+public class MemberV1 {
+    private String name;
+    private Address address;
+
+    public MemberV1(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberV1{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
