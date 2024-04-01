73fd17d51ba440047a8a0f698d58334883e8d360
hyowonsong <woniwoni30@gmail.com>
2024-04-01 16:56:11 +0900

    [Add] Chapter7. 중첩 클래스, 내부 클래스

---
+++ src/Chapter7/nested/inner/ex1/Car.java
@@ -0,0 +1,26 @@
+package Chapter7.nested.inner.ex1;
+
+public class Car {
+    private String model;
+    private int chargeLevel;
+    private Engine engine;
+
+    public Car(String model, int chargeLevel) {
+        this.model = model;
+        this.chargeLevel = chargeLevel;
+        this.engine = new Engine(this); // 여기 다름
+    }
+    //Engine에서만 사용하는 메서드
+    public String getModel() {      // getModel, getChargeLvel 없어져도 된다.
+        return model;
+    }
+    //Engine에서만 사용하는 메서드
+    public int getChargeLevel() {
+        return chargeLevel;
+    }
+    public void start() {
+        engine.start();
+        System.out.println(model + " 시작 완료");
+    }
+
+}
