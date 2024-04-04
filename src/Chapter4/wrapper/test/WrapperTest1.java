e69b0dc39e25c986d157e348d95539a27bbe2d81
hyowonsong <woniwoni30@gmail.com>
2024-04-04 17:05:53 +0900

    [Add] Chapter4,5 래퍼클래스, ENUM 추가

--- src/Chapter4/wrapper/test/WrapperTest1.java
+++ src/Chapter4/wrapper/test/WrapperTest1.java
@@ -1,15 +1,16 @@
 package Chapter4.wrapper.test;
 
 // 문자로 입력된 `str1`, `str2` 두 수의 합을 구하자
+// parseInt : 문자열 기본형으로 반환
 public class WrapperTest1 {
     public static void main(String[] args) {
         String str1 = "10";
         String str2 = "20";
 
         // 코드 작성
-        Integer num1 = Integer.parseInt(str1);
-        Integer num2 = Integer.parseInt(str2);
-        int sum = num1 + num2;
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        int sum = num1+num2;
 
         System.out.println("두 수의 합: " + sum);
     }
